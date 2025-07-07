7e86bd78b296253ff1814c3be2edeef578666e1c
sethw <sethdwoolley@gmail.com>
2025-07-07 17:18:05 +0100

    Add basedpyright as installed LSP

--- init.lua
+++ init.lua
@@ -204,6 +204,7 @@ require('lazy').setup({
         local ensure_installed = vim.tbl_keys(servers or {})
         vim.list_extend(ensure_installed, {
           'stylua', -- Used to format Lua code
+          'basedpyright', -- Python LSP
         })
         require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
