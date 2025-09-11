3f58ddecf4655bea675d0d4c5f0b30219089932b
Seth Woolley <seth.w.public@proton.me>
2025-09-11 11:29:39 +0100

    wezterm: Add alt+1-9 for tab switching

--- wezterm/config/bindings.lua
+++ wezterm/config/bindings.lua
@@ -32,6 +32,15 @@ local keys = {
     { key = ']',    mods = mod.SUPER,     action = act.ActivateTabRelative(1) },
     { key = '[',    mods = mod.SUPER_REV, action = act.MoveTabRelative(-1) },
     { key = ']',    mods = mod.SUPER_REV, action = act.MoveTabRelative(1) },
+    { key = '1',    mods = mod.SUPER,     action = act.ActivateTab(0) },
+    { key = '2',    mods = mod.SUPER,     action = act.ActivateTab(1) },
+    { key = '3',    mods = mod.SUPER,     action = act.ActivateTab(2) },
+    { key = '4',    mods = mod.SUPER,     action = act.ActivateTab(3) },
+    { key = '5',    mods = mod.SUPER,     action = act.ActivateTab(4) },
+    { key = '6',    mods = mod.SUPER,     action = act.ActivateTab(5) },
+    { key = '7',    mods = mod.SUPER,     action = act.ActivateTab(6) },
+    { key = '8',    mods = mod.SUPER,     action = act.ActivateTab(7) },
+    { key = '9',    mods = mod.SUPER,     action = act.ActivateTab(8) },
 
     -- tab: title
     {
