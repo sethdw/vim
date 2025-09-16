0aa897e9b10b1a022fe8247de5045f60450ba35d
Seth Woolley <seth.w.public@proton.me>
2025-09-16 13:09:22 +0100

    nvim: Switch to copilot.lua
    Has a couple more funky features

--- nvim/init.lua
+++ nvim/init.lua
@@ -348,15 +348,17 @@ require('lazy').setup({
 
     -- copilot
     {
-      'github/copilot.vim',
-      config = function()
-        vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
-          expr = true,
-          replace_keycodes = false
-        })
-        vim.g.copilot_no_tab_map = true
-        vim.g.copilot_suggest = false
-      end,
+      'zbirenbaum/copilot.lua',
+      opts = {
+        panel = { enabled = false },
+        suggestion = {
+            enabled = true,
+            keymap = {
+                accept = "<C-J>",
+                accept_line = "<C-H>",
+            },
+        },
+      },
     },
     -- extend a/i textobjects
     {
